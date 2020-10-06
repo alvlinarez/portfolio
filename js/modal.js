c39c25700ce2137c9431eb3670bf9ef600290efe
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-10-06 13:00:50 -0500

    All Project Modals Finished

--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,23 @@
-// Get the modal
-const modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-const btn = document.getElementById('myBtn');
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
+function openModal(id = 'videosModal') {
+  // Get the modal
+  const modal = document.getElementById(id);
   modal.style.display = 'block';
-  document.body.style.overflow = 'hidden'; // ADD THIS LINE
-  document.body.style.height = '100%'; // ADD THIS LINE
-};
+  document.body.style.overflow = 'hidden';
+  document.body.style.height = '100%';
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+      document.body.style.overflow = 'auto';
+      document.body.style.height = 'auto';
+    }
+  };
+}
+
+function closeModal(id = 'videosModal') {
+  const modal = document.getElementById(id);
   modal.style.display = 'none';
-  document.body.style.overflow = 'auto'; // ADD THIS LINE
-  document.body.style.height = 'auto'; // ADD THIS LINE
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-    document.body.style.overflow = 'auto'; // ADD THIS LINE
-    document.body.style.height = 'auto'; // ADD THIS LINE
-  }
-};
+  document.body.style.overflow = 'auto';
+  document.body.style.height = 'auto';
+}
